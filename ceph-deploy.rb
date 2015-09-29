0eaf49d9b4fd2a831e7bfb039a62995dcab3b656
Anirvan BASU <Anirvan.BASU@INRIA.fr>
2015-09-29 20:54:02 +0200

    corrections & modifications to ceph-deploy.rb & README.md

--- ceph-deploy.rb
+++ ceph-deploy.rb
@@ -289,21 +289,32 @@ puts "Preparing & activating OSDs..."
 
 # mkdir, Prepare & Activate each OSD
 osdIndex = 0 # change to check if osdIndex file exists, then initialise from there
-osdNodes.each_with_index do |node, index|
-     nodeShort = node.split(".").first
+osdNodes.each do |node| # loop over all OSD nodes
+     nodeShort = node.split(".").first       # the shortname of the node
+     g5kCluster = nodeShort.split("-").first # the G5K cluster of the node
      Cute::TakTuk.start([node], :user => "root") do |tak|
-          tak.exec!("rm -rf /osd#{index}")
-          tak.exec!("mkdir /osd#{index}")
+          result = tak.exec!("curl -kn 'https://api.grid5000.fr/sid/sites/#{argSite}/clusters/#{g5kCluster}/nodes/#{nodeShort}'")
+          output = result[node][:output]
+puts output
+          parsedOutput = JSON.parse(output)
+          storageDevices = parsedOutput["storage_devices"]
+          storageDevices.each do |storageDev| # loop over each physical disc
+             device = storageDev["device"]
+             unless device == "sda" # deploy OSD only on partition /dev/sda5
+                tak.exec!("ceph-deploy osd prepare #{nodeShort}:/dev/#{device}5")
+                tak.exec!("ceph-deploy osd activate #{nodeShort}:/dev/#{device}5")
+                puts "Prepared & activated OSD: #{nodeShort}:/dev/#{device}5\n\n"
+             else  # deploy OSD on all discs as /dev/sdb, /dev/sdc, ...
+                tak.exec!("ceph-deploy osd prepare #{nodeShort}:/dev/#{device}")
+                tak.exec!("ceph-deploy osd activate #{nodeShort}:/dev/#{device}")
+                puts "Prepared & activated OSD: #{nodeShort}:/dev/#{device}\n\n"
+             end
+             osdIndex += 1
+          end # loop over each physical disc
           tak.loop()
      end
+end # loop over all OSD nodes
 
-     Cute::TakTuk.start([monitor], :user => "root") do |tak|
-          tak.exec!("ceph-deploy osd prepare #{nodeShort}:/osd#{index}")
-          tak.exec!("ceph-deploy osd activate #{nodeShort}:/osd#{index}")
-          tak.loop()
-     end
-     osdIndex = index
-end
 
 # Write to osdIndex & osdList files locally
 confFile = File.open("osdIndex", "w"){ |file|
@@ -373,39 +384,6 @@ Cute::TakTuk.start([monitor], :user => "root") do |tak|
         puts "Ceph cluster up and running. In state 'active+clean'.\n"
      end
      tak.loop()
-end
-
-
-
-
-
-
-# mkdir, Prepare & Activate each OSD
-osdIndex = 0 # change to check if osdIndex file exists, then initialise from there
-osdNodes.each_with_index do |node, index|
-     nodeShort = node.split(".").first       # the shortname of the node
-     g5kCluster = nodeShort.split("-").first # the G5K cluster of the node
-     Cute::TakTuk.start([node], :user => "root") do |tak|
-          result = tak.exec!("curl -kn 'https://api.grid5000.fr/sid/sites/#{argSite}/clusters/#{g5kCluster}/nodes/#{nodeShort}'")
-          output = result[node][:output]
-          parsedOutput = JSON.parse(output)
-          storageDevices = parsedOutput["storage_devices"]
-#         puts "#{output} \n\n\n #{parsedOutput} \n\n\n #{storageDevices}"
-          storageDevices.each do | storageDev |
-             device = storageDev["device"]
-             if device == "sda" # deploy OSD only on partition /dev/sda5
-                tak.exec!("ceph-deploy osd prepare #{nodeShort}:/dev/#{device}5")
-                tak.exec!("ceph-deploy osd activate #{nodeShort}:/dev/#{device}5")
-                puts "Prepared & activated OSD: #{nodeShort}:/dev/#{device}5\n\n"
-             else  # deploy OSD on all discs as /dev/sdb, /dev/sdc, ...
-                tak.exec!("ceph-deploy osd prepare #{nodeShort}:/dev/#{device}")
-                tak.exec!("ceph-deploy osd activate #{nodeShort}:/dev/#{device}")
-                puts "Prepared & activated OSD: #{nodeShort}:/dev/#{device}\n\n"
-             end
-          end
-          tak.loop()
-     end
-     osdIndex = index
 end
 
 
