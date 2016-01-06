74517b3443f40840744752b41e3d4dcf748665b0
Anirvan BASU <Anirvan.BASU@INRIA.fr>
2016-01-06 14:19:27 +0100

    Commit related to independent client in cephRBD.rb

--- cephDeploy.rb
+++ cephDeploy.rb
@@ -158,7 +158,8 @@ puts "OSDs on: #{osdNodes}" + "\n"
 puts "Doing pre-flight checklist..."
 # Add (release) Keys to each Ceph node
 Cute::TakTuk.start(nodes, :user => "root") do |tak|
-     tak.put("/home/abasu/public/release.asc", "/root/release.asc")
+     result = tak.put("/home/abasu/public/release.asc", "/root/release.asc")
+puts result
      tak.exec!("cat ./release.asc  | apt-key add -")
      tak.loop()
 end
@@ -171,7 +172,8 @@ ceph_update =  'http://ceph.com/debian-#{argRelease}/ wheezy main'
 Cute::TakTuk.start(nodes, :user => "root") do |tak|
 #     tak.exec!("echo deb #{ceph_extras}  | sudo tee /etc/apt/sources.list.d/ceph-extras.list")
 #     tak.exec!("echo deb #{ceph_update}  | sudo tee /etc/apt/sources.list.d/ceph.list")
-     tak.exec!("export http_proxy=http://proxy:3128; export https_proxy=https://proxy:3128; sudo apt-get update -y && sudo apt-get install -y ceph-deploy")
+     result = tak.exec!("export http_proxy=http://proxy:3128; export https_proxy=https://proxy:3128; sudo apt-get update -y && sudo apt-get install -y ceph-deploy")
+puts result
      tak.loop()
 end
 
