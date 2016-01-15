1d25571afb283a73c06675c23f134b68b01fee8c
Anirvan BASU <Anirvan.BASU@INRIA.fr>
2016-01-15 10:04:03 +0100

    Update on Ceph production

--- cephRBD.rb
+++ cephRBD.rb
@@ -148,9 +148,7 @@ Cute::TakTuk.start([client], :user => "root") do |tak|
            # Check if RBD is already created, may contain data
            resultPool = tak.exec!("rados -c /root/prod/ceph.conf --id #{user} --pool #{userPool} ls")
           rbdList = result[client][:output].split("\n")
-puts rbdList
           rbdList.each do |rbd|  # logic: it will take the alphabetic-last pool from user
-puts rbd
              if rbd.include? "#{argRBDName}"
                 userRBD = rbd
 puts userRBD
