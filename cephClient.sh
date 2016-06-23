5aa192d72a8bd6da35c9f8a74dea56b91abbbcae
Anirvan BASU <Anirvan.BASU@INRIA.fr>
2016-06-23 18:30:35 +0200

    updated scripts for Hadoop on managed Ceph

--- cephClient.sh
+++ cephClient.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Get the client keyring for Ceph production for current user. Store in /tmp on frontend.
-curl -k https://api.grid5000.fr/sid/storage/ceph/auths/$USER.keyring | cat - > /tmp/ceph.client.$USER.keyring
+if [ $1 == $2 ] ; then
+   echo "$0: managed Ceph site expected - rennes or nantes"
+   exit 3
+fi
+curl -k https://api.grid5000.fr/sid/sites/$1/storage/ceph/auths/$USER.keyring | cat - > /tmp/ceph.client.$USER.keyring
