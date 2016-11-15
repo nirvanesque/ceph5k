23120108d0543176d5c44bc6475f165e89857da5
Anirvan BASU <Anirvan.BASU@INRIA.fr>
2016-11-15 15:42:47 +0100

    modified cephDeploy, cephClient, cephmanaged + adding functions - 4

--- helpers/abstractions.rb
+++ helpers/abstractions.rb
@@ -56,29 +56,29 @@ def readOptions(scriptDir, currentConfigFile, scriptName)
      case scriptName
         when "cephDeploy"
            banner <<-EOS
-         cephDeploy is a script for deploying a Ceph cluster on reserved nodes.
+  cephDeploy is a script for deploying a Ceph cluster on reserved nodes.
 
-         Usage: 
-                cephDeploy [options]
-         where [options] are:
+  Usage: 
+         cephDeploy [options]
+  where [options] are:
          EOS
 
         when "cephClient"
            banner <<-EOS
-         cephClient is a script for creating clients to access a deployed Ceph cluster.
+  cephClient is a script for creating clients to access a deployed Ceph cluster.
 
-         Usage: 
-                cephClient [options]
-         where [options] are:
+  Usage: 
+         cephClient [options]
+  where [options] are:
          EOS
 
         when "cephManaged"
            banner <<-EOS
-         cephManaged is a script for creating clients to access a Managed Ceph cluster.
+  cephManaged is a script for creating clients to access a Managed Ceph cluster.
 
-         Usage: 
-                cephManaged [options]
-         where [options] are:
+  Usage: 
+         cephManaged [options]
+  where [options] are:
          EOS
 
         else
@@ -88,8 +88,13 @@ def readOptions(scriptDir, currentConfigFile, scriptName)
 
      end # case scriptName
 
-     opt :ignore, "Ignore incorrect values"
-     opt :jobid, "Oarsub ID of the job", :default => 0
+  opt :ignore, "Ignore incorrect values"
+  opt :jobid, "Oarsub ID of the client job", :default => 0
+  opt :walltime, "Wall time for reservation", :type => String, :default => defaults["walltime"]
+
+     case scriptName
+
+        when "cephDeploy" # options specific for script cephDeploy
      opt :'job-name', "Name of Grid'5000 job if already created", :type =>    String, :default => defaults["job-name"]
      opt :site, "Grid 5000 site for deploying Ceph cluster", :type => String, :default => defaults["site"]
      opt :cluster, "Grid 5000 cluster in specified site", :type => String, :default => defaults["cluster"]
@@ -102,6 +107,51 @@ def readOptions(scriptDir, currentConfigFile, scriptName)
      opt :'multi-osd', "Multiple OSDs on each node", :default => defaults["multi-osd"]
      opt :'file-system', "File System to be formatted on OSDs", :type => String, :default => defaults["file-system"]
      opt :'conf-file', "Configuration file to be used for deployment", :type => String, :default => currentConfigFile
+
+
+        when "cephClient" # options specific for script cephClient
+  opt :'job-name', "Grid'5000 job name for dedicated Ceph cluster", :type => String, :default => defaults["job-name"]
+  opt :site, "Grid 5000 site where dedicated Ceph cluster is deployed", :type => String, :default => defaults["site"]
+  opt :walltime, "Wall time for Ceph clients reservation", :type => String, :default => defaults["walltime"]
+
+  opt :'job-client', "Grid'5000 job name for Ceph clients", :type => String, :default => defaults["job-client"]
+  opt :'client-site', "Grid'5000 site for deploying Ceph clients", :type => String, :default => defaults["client-site"]
+  opt :'client-cluster', "Grid'5000 cluster for deploying Ceph clients", :type => String, :default => defaults["client-cluster"]
+  opt :'num-client', "Number of Ceph client(s)", :default => defaults["num-client"]
+  opt :'env-client', "Grid'5000 environment for client", :type => String, :default => defaults["env-client"]
+  opt :'file', "File with list of predeployed clients, similar as in kadeploy3", :type => String, :default => ""
+  opt :'only-deploy', "Only deploy linux but don't configure Ceph client", :default => defaults["only-deploy"]
+
+  opt :'pool-name', "Pool name on Ceph cluster (userid_ prepended)", :type => String, :default => defaults["client-pool-name"]
+  opt :'rbd-name', "RBD name on Ceph pool (userid_ prepended)", :type => String, :default => defaults["client-rbd-name"]
+  opt :'rbd-size', "RBD size on Ceph pool", :default => defaults["client-rbd-size"]
+  opt :release, "Ceph Release name", :type => String, :default => defaults["release"]
+  opt :'file-system', "File System to be formatted on created RBDs", :type => String, :default => defaults["file-system"]
+  opt :'mnt-depl', "Mount point for RBD on deployed cluster", :type => String, :default => defaults["mnt-depl"]
+
+
+        when "cephManaged" # options specific for script cephManaged
+  opt :'job-client', "Grid'5000 job name for Ceph clients", :type => String, :default => defaults["job-client"]
+  opt :'file', "File with clients list, same option as in kadeploy3", :type => String, :default => ""
+  opt :'client-site', "Grid 5000 site for deploying Ceph clients", :type => String, :default => defaults["client-site"]
+  opt :'client-cluster', "Grid 5000 cluster for clients", :type => String, :default => defaults["client-cluster"]
+  opt :'env-client', "G5K environment for client", :type => String, :default => defaults["env-client"]
+
+  opt :'managed-cluster', "site for managed Ceph cluster: 'rennes' or 'nantes'", :type => String, :default => defaults["managed-cluster"]
+  opt :'multi-client', "Multiple clients to access Ceph Managed cluster", :default => defaults["multi-client"]
+  opt :'num-client', "Nodes in Ceph Client cluster", :default => defaults["num-client"]
+  opt :'no-deployed', "Not using any deployed Ceph cluster", :default => defaults["no-deployed"]
+
+  opt :'pool-name', "Pool name on Ceph cluster (userid_ prepended)", :type => String, :default => defaults["client-pool-name"]
+  opt :'rbd-name', "RBD name for Ceph pool (userid_ prepended)", :type => String, :default => defaults["client-rbd-name"]
+  opt :'rbd-size', "RBD size on Ceph pool", :default => defaults["client-rbd-size"]
+  opt :'file-system', "File System to be formatted on created RBDs", :type => String, :default => defaults["file-system"]
+  opt :'rbd-list-file', "YAML file with RBD list. No. of RBDs must be same as no. of clients", :type => String, :default => nil
+  opt :release, "Ceph Release name", :type => String, :default => defaults["release"]
+  opt :'mnt-prod', "Mount point for RBD on managed cluster", :type => String, :default => defaults["mnt-prod"]
+
+
+     end # case scriptName
 
    end
 
